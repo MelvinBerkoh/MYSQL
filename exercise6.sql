eacacf67c456f11c11d1a78c34f233b5d4492be8
Melvin <106451111+MakaveliMelly@users.noreply.github.com>
2023-12-14 10:59:12 -0500

    added addition comments

--- exercise6.sql
+++ exercise6.sql
@@ -1,6 +1,9 @@
 /* Exercise 6 
  get the customers whose addresses contain TRAIL or Avenue 
  Then get ones whose phone numbers end with 9*/
+-- Like operator to compare to a certain pattern 
+-- % any number of character
+-- _ single character or a specified number of characters
 SELECT first_name,
   last_name,
   address
