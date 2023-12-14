618883b6661009fae3d6bae6835914eff8725f00
Melvin <106451111+MakaveliMelly@users.noreply.github.com>
2023-12-14 10:01:00 -0500

    update to the logic behind previous code

--- exercise2.sql
+++ exercise2.sql
@@ -2,8 +2,8 @@
 /* this returns null because there isnt any data that matches*/
 SELECT order_id
 FROM orders O
-WHERE shipped_date = '2023-12-14';
+WHERE shipped_date >= '2023-01-01';
 /* This returns zero using the count function to avoid having a null there*/
 SELECT COUNT(order_id) As 'Number of orders this year'
 FROM orders O
-WHERE shipped_date = '2023-12-14';
+WHERE shipped_date >= '2023-01-01';
